12ac5b8d9877c4d31eb8f22ee739d148d5ef6e14
Sherrifdeen Tijani <43505184+tsherrifdeen@users.noreply.github.com>
2023-11-24 10:32:20 +0100

    Edited delete buttons

--- src/overview/candidateCard.jsx
+++ src/overview/candidateCard.jsx
@@ -1,66 +1,56 @@
-import React from "react"
-import { deleteCandidate } from "../shared/Backend"
+import React from "react";
+import { deleteCandidate } from "../shared/Backend";
 
 function CandidateLayout({ candidate }) {
-    const {
-        candidate_department,
-        candidate_level,
-        candidate_name,
-        picture,
-        candidate_id,
-        votes,
-    } = candidate
-    const handleCandidateDelete = async () => {
-        if (
-            window.confirm(`Are you sure you want to delete ${candidate_name}?`)
-        ) {
-            const res = await deleteCandidate({ candidateId: candidate_id })
-            if (res.status === 201) {
-                window.location.reload()
-            }
-        }
+  const {
+    candidate_department,
+    candidate_level,
+    candidate_name,
+    picture,
+    candidate_id,
+    votes,
+  } = candidate;
+  const handleCandidateDelete = async () => {
+    if (window.confirm(`Are you sure you want to delete ${candidate_name}?`)) {
+      const res = await deleteCandidate({ candidateId: candidate_id });
+      if (res.status === 201) {
+        window.location.reload();
+      }
     }
-    return (
-		<div
-			className={`relative w-full max-w-sm md:border border-gray-200 rounded-lg md:shadow-md`}
-		>
-			<div className='flex flex-col items-center pt-8 pb-10'>
-				<img
-					className='object-cover w-24 h-24 mb-3 bg-white rounded-full shadow-lg'
-					src={
-						picture?.startsWith("http")
-							? picture
-							: "/pfp.svg"
-					}
-					alt='profile'
-				/>
-				<h5 className='mb-1 text-xl font-medium text-light-text-primary'>
-					{candidate_name ||
-						"No candidate name available"}
-				</h5>
-				<span className='text-sm text-light-text-muted'>
-					{candidate_department}
-				</span>
-				<span className='text-sm text-light-text-muted'>
-					{candidate_level + " "}level
-				</span>
-				<span className='text-light-text-muted'>
-					Votes:{" "}
-					<span className='font-semibold'>
-						{votes}
-					</span>
-				</span>
-				<div className='mt-4'>
+  };
+  return (
+    <div
+      className={`relative w-full max-w-sm md:border border-gray-200 rounded-lg md:shadow-md`}
+    >
+      <div className="flex flex-col items-center pt-8 pb-10">
+        <img
+          className="object-cover w-24 h-24 mb-3 bg-white rounded-full shadow-lg"
+          src={picture?.startsWith("http") ? picture : "/pfp.svg"}
+          alt="profile"
+        />
+        <h5 className="mb-1 text-xl font-medium text-light-text-primary">
+          {candidate_name || "No candidate name available"}
+        </h5>
+        <span className="text-sm text-light-text-muted">
+          {candidate_department}
+        </span>
+        <span className="text-sm text-light-text-muted">
+          {candidate_level + " "}level
+        </span>
+        <span className="text-light-text-muted">
+          Votes: <span className="font-semibold">{votes}</span>
+        </span>
+        {/* <div className='mt-4'>
 					<button
 						className={`inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-500 border border-gray-300 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-gray-200 tracking-widest`}
 						onClick={handleCandidateDelete}
 					>
 						DELETE
 					</button>
-				</div>
-			</div>
-		</div>
-    );
+				</div> */}
+      </div>
+    </div>
+  );
 }
 
-export default CandidateLayout
+export default CandidateLayout;
--- src/overview/overview.jsx
+++ src/overview/overview.jsx
@@ -1,40 +1,40 @@
-import React from "react"
-import { useSelector } from "react-redux"
-import PositionLayout from "./positionCard"
+import React from "react";
+import { useSelector } from "react-redux";
+import PositionLayout from "./positionCard";
 
 function Overview() {
-    let positions = useSelector((state) => state.position.positions)
-    let candidates = useSelector((state) => state.candidate.candidates)
-    let totalUsers = useSelector((state) => state.app.totalUsers)
-    let usersVoted = useSelector((state) => state.app.usersVoted)
+  let positions = useSelector((state) => state.position.positions);
+  let candidates = useSelector((state) => state.candidate.candidates);
+  let totalUsers = useSelector((state) => state.app.totalUsers);
+  let usersVoted = useSelector((state) => state.app.usersVoted);
 
-    const pStyle = "text-lg"
+  const pStyle = "text-lg";
 
-    let positionComponents = positions.map((position) => {
-        return (
-            <PositionLayout
-                key={position.position_id}
-                position={position}
-                candidates={candidates}
-            />
-        )
-    })
-
+  let positionComponents = positions.map((position) => {
     return (
-        <div className="w-full">
-            <div className="mt-10"></div>
-            <p className={pStyle}>
-                <u>Summary</u>
-            </p>
-            <p className={pStyle}>
-                Total number of voters in database: <b>{totalUsers}</b>
-            </p>
-            <p className={pStyle}>
-                Total number of users voted: <b>{usersVoted}</b>{" "}
-            </p>
-            <div className="divide-y-4 ">{positionComponents}</div>
-        </div>
-    )
+      <PositionLayout
+        key={position.position_id}
+        position={position}
+        candidates={candidates}
+      />
+    );
+  });
+
+  return (
+    <div className="w-full">
+      <div className="mt-10"></div>
+      <p className={pStyle}>
+        <u>Summary</u>
+      </p>
+      {/* <p className={pStyle}>
+        Total number of registered voters: <b>{totalUsers}</b>
+      </p> */}
+      <p className={pStyle}>
+        Total number of votes: <b>{usersVoted}</b>{" "}
+      </p>
+      <div className="divide-y-4 ">{positionComponents}</div>
+    </div>
+  );
 }
 
-export default Overview
+export default Overview;
